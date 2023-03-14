6f0b1948e5109c93b3769763399a67c57382f59c
DarryZh <87570095+DarryZh@users.noreply.github.com>
2023-03-14 14:40:28 +0800

    Update pinmap.h

--- bsp/borad/pinmap.h
+++ bsp/borad/pinmap.h
@@ -3,12 +3,5 @@
 
 #include "hal/gpio_types.h"
 
-//lora
-#define LORA_PIN_NUM_MISO    GPIO_NUM_2
-#define LORA_PIN_NUM_MOSI    GPIO_NUM_7
-#define LORA_PIN_NUM_CLK     GPIO_NUM_6
-#define LORA_PIN_NUM_CS      GPIO_NUM_10
-#define LORA_PIN_NUM_BUSY    GPIO_NUM_4
-#define LORA_PIN_NUM_DIO1    GPIO_NUM_5
 
 #endif
