e126cd3023d31e7babf03e9ae6e95808be5326e4
DarryZh <87570095+DarryZh@users.noreply.github.com>
2023-03-14 14:13:43 +0800

    Create pinmap.h

---
+++ bsp/borad/pinmap.h
@@ -0,0 +1,14 @@
+#ifndef __PINMAP_H__
+#define __PINMAP_H__
+
+#include "hal/gpio_types.h"
+
+//lora
+#define LORA_PIN_NUM_MISO    GPIO_NUM_2
+#define LORA_PIN_NUM_MOSI    GPIO_NUM_7
+#define LORA_PIN_NUM_CLK     GPIO_NUM_6
+#define LORA_PIN_NUM_CS      GPIO_NUM_10
+#define LORA_PIN_NUM_BUSY    GPIO_NUM_4
+#define LORA_PIN_NUM_DIO1    GPIO_NUM_5
+
+#endif
